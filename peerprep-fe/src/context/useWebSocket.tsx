cbd8fad287b97093edd4a3387a9c1a623c495c96
fungusta <peterjosephfung@gmail.com>
2024-10-11 15:39:37 +0800

    fix bug

--- peerprep-fe/src/context/useWebSocket.tsx
+++ peerprep-fe/src/context/useWebSocket.tsx
@@ -1,53 +1,63 @@
-import { 
-    type PropsWithChildren,
-    type FC, 
-    createContext, 
-    useContext, 
-    useEffect, 
-    useState 
+import {
+  type PropsWithChildren,
+  type FC,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  useMemo,
 } from 'react';
 
 // Defines the types of data stored in the context
 type WebSocketContextType = {
-    socket: WebSocket | null;
-    messages: string[];
-    loading: boolean;
-    sendMessage: (message: string) => void;
+  socket: WebSocket | null;
+  messages: string[];
+  loading: boolean;
+  sendMessage: (message: string) => void;
 };
 
 const WebSocketContext = createContext<WebSocketContextType | null>(null);
 
 export const WebSocketProvider: FC<PropsWithChildren> = ({ children }) => {
-    const [socket, setSocket] = useState<WebSocket | null>(null);
-    const [loading, setLoading] = useState(true);
-    const [messages, setMessages] = useState<string[]>([]);
-    
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [messages, setMessages] = useState<string[]>([]);
 
-    useEffect(() => {
-        const ws = new WebSocket('ws://localhost:8080') // Change if needed
-        setSocket(ws);
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8080'); // Change if needed
+    setSocket(ws);
 
-        ws.onopen = () => {
-            setLoading(false);
-        }
-
-        ws.onmessage = (event) => {
-            setMessages((prevMessages) => [...prevMessages, event.data]);
-        };
-
-        return () => {
-            ws.close();
-        };
-    }, []);
-
-    const sendMessage = (message: string) => {
-        if (socket && socket.readyState === WebSocket.OPEN) {
-            socket.send(message);
-        }
+    ws.onopen = () => {
+      setLoading(false);
     };
 
-    return <WebSocketContext.Provider value={{ socket, loading, messages, sendMessage }}>{children}</WebSocketContext.Provider>;
+    ws.onmessage = (event) => {
+      const newMessage: string = event.data as string;
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+    };
 
-}
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  const sendMessage = (message: string) => {
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      socket.send(message);
+    }
+  };
+
+  const contextValue = useMemo(
+    () => ({
+      socket,
+      loading,
+      messages,
+      sendMessage,
+    }),
+    [socket, loading, messages]
+  );
+
+  return <WebSocketContext.Provider value={contextValue}>{children}</WebSocketContext.Provider>;
+};
 
 export const useWebSocket = () => useContext(WebSocketContext);
