4a82d9211ec2b83759299ef734205278cd629fc5
Tan Lip Wei <53000252+lipwei1808@users.noreply.github.com>
2024-10-19 16:00:20 +0800

    Update fe

--- peerprep-fe/src/pages/find-match/index.tsx
+++ peerprep-fe/src/pages/find-match/index.tsx
@@ -1,12 +1,21 @@
 import { Fragment, useState, useCallback, useEffect, useRef } from 'react';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 
 import { performMatching, pollMatchingStatus } from '@/api/matching';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
 import {
   Form,
   FormControl,
@@ -15,7 +24,7 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
-import Loading from '@/components/ui/loading/loading';
+import BlockSpinning from '@/components/ui/loading/blockSpinning';
 import {
   Select,
   SelectContent,
@@ -23,6 +32,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import { useToast } from '@/components/ui/toast/use-toast';
 import { useSession } from '@/context/useSession';
 import { Role } from '@/types/user';
 
@@ -33,18 +43,20 @@ const FormSchema = z.object({
 
 const DIFFICULTIES = ['all', 'easy', 'medium', 'hard'];
 const TOPICS = ['all', 'dynamic programming', 'tree', 'string', 'arrays'];
-const MAX_POLL_COUNT = 30; // Maximum number of poll attempts
-const POLL_INTERVAL = 5000; // Poll every 5 seconds
+const MAX_POLL_COUNT = 3; // Maximum number of poll attempts
+const POLL_INTERVAL = 3000; // Poll every 5 seconds
 
 const FindMatchPage = () => {
   const [difficulty, setDifficulty] = useState(+new Date());
   const [topic, setTopic] = useState(+new Date());
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [matchRequestId, setMatchRequestId] = useState<string | null>(null);
   const [pollCount, setPollCount] = useState(0);
   const [error, setError] = useState('');
   const { sessionData } = useSession();
   const pollIntervalId = useRef<ReturnType<typeof setInterval> | null>(null);
+  const { toast } = useToast();
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
@@ -79,159 +91,197 @@ const FindMatchPage = () => {
     }
   };
 
-  const pollStatus = useCallback(async () => {
-    if (!matchRequestId) return;
-    if (pollCount >= MAX_POLL_COUNT) {
-      setLoading(false);
-      setMatchRequestId(null);
-      setError('Failed to find a match. Please try again.');
-      return;
-    }
-
-    try {
-      const hasMatch = await pollMatchingStatus(matchRequestId);
-
-      if (hasMatch) {
+  const pollStatus = useCallback(
+    async (matchRequestId: string) => {
+      if (pollCount >= MAX_POLL_COUNT) {
         setLoading(false);
         setMatchRequestId(null);
-      } else {
-        setPollCount((prevCount) => prevCount + 1);
+        setError('Failed to find a match. Please try again.');
+        return;
       }
-    } catch (error) {
-      setLoading(false);
-      setMatchRequestId(null);
-      setPollCount(0);
-      if (pollIntervalId.current !== null) {
-        clearInterval(pollIntervalId.current);
+
+      try {
+        const hasMatch = await pollMatchingStatus(matchRequestId);
+
+        if (hasMatch) {
+          setLoading(false);
+          setMatchRequestId(null);
+          toast({ description: 'Found a match, redirecting you to collaborative page' });
+          router.push('/question');
+        } else {
+          setPollCount((prevCount) => prevCount + 1);
+        }
+      } catch (error) {
+        setLoading(false);
+        setMatchRequestId(null);
+        if (pollIntervalId.current !== null) {
+          clearInterval(pollIntervalId.current);
+        }
+        setError('An error occurred while checking match status. Please try again.');
       }
-      setError('An error occurred while checking match status. Please try again.');
-    }
-  }, [matchRequestId, pollCount, setError]);
+    },
+    [pollCount, router, toast, setError]
+  );
 
   // eslint-disable-next-line consistent-return
   useEffect(() => {
-    pollIntervalId.current = setInterval(() => {
-      pollStatus();
-    }, POLL_INTERVAL);
-    // pollStatus(); // call on mount
+    if (matchRequestId) {
+      pollIntervalId.current = setInterval(() => {
+        pollStatus(matchRequestId);
+      }, POLL_INTERVAL);
+    }
+
     if (pollIntervalId.current !== null) {
       return () => clearInterval(pollIntervalId.current as ReturnType<typeof setInterval>);
     }
-  }, [matchRequestId, pollCount, pollStatus, setError]);
+  }, [matchRequestId, pollStatus]);
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-y-8">
-      <div className="text-4xl font-bold">PeerPrep</div>
-      <div className="relative w-72 rounded-lg bg-gray-100 p-4">
-        {loading && (
-          <Loading className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
-        )}
-        {error && <div className="mb-4 rounded-md bg-red-100 p-4 text-red-700">{error}</div>}
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <div className={`flex flex-col gap-y-5 ${loading ? 'opacity-50' : ''}`}>
-              <FormField
-                control={form.control}
-                name="difficulty"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="itms-center flex justify-between">
-                      <span>Difficulty</span>
-                      <Badge
-                        onClick={() => {
-                          form.resetField('difficulty');
-                          setDifficulty(+new Date());
-                        }}
-                        variant="outline"
-                        className="cursor-pointer hover:bg-accent"
+    <>
+      <Dialog open={loading} onOpenChange={setLoading}>
+        <DialogContent
+          // To prevent accidental closure of the dialog when clicking outside bg
+          onInteractOutside={(e) => e.preventDefault()}
+          onPointerDownOutside={(e) => e.preventDefault()}
+          hideClose
+        >
+          <DialogHeader>
+            <DialogTitle>Finding a match now...</DialogTitle>
+            <DialogDescription>
+              Closing this popup ends the search and you will need to find match again.
+              <BlockSpinning height="100" width="100" />
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button
+              variant="destructive"
+              onClick={() => {
+                setLoading(false);
+                if (pollIntervalId.current !== null) {
+                  clearInterval(pollIntervalId.current);
+                }
+              }}
+            >
+              Cancel search
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <div className="layout">
+        <div className="flex flex-col items-start gap-2 py-8">
+          <h1 className="text-3xl font-bold">Find a Match</h1>
+          <p className="text-lg font-light text-foreground">
+            Select your preferred difficulty and topic you would like to practice.
+          </p>
+        </div>
+        <div className="relative w-[400px]">
+          {error && <div className="mb-4 rounded-md bg-red-100 p-4 text-red-700">{error}</div>}
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className={`flex flex-col gap-y-5 ${loading ? 'opacity-50' : ''}`}>
+                <FormField
+                  control={form.control}
+                  name="difficulty"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="itms-center flex justify-between">
+                        <span>Difficulty</span>
+                        <Badge
+                          onClick={() => {
+                            form.resetField('difficulty');
+                            setDifficulty(+new Date());
+                          }}
+                          variant="outline"
+                          className="cursor-pointer hover:bg-accent"
+                        >
+                          Clear
+                        </Badge>
+                      </FormLabel>
+                      <Select
+                        disabled={loading}
+                        key={difficulty}
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
                       >
-                        Clear
-                      </Badge>
-                    </FormLabel>
-                    <Select
-                      disabled={loading}
-                      key={difficulty}
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder="Select a verified email to display" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        {DIFFICULTIES?.length ? (
-                          DIFFICULTIES?.map((d) => (
-                            <Fragment key={d}>
-                              <SelectItem className="cursor-pointer" value={d}>
-                                {d}
-                              </SelectItem>
-                            </Fragment>
-                          ))
-                        ) : (
-                          <div>Error loading schools</div>
-                        )}
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="topic"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="itms-center flex justify-between">
-                      <span>Topic</span>
-                      <Badge
-                        onClick={() => {
-                          form.resetField('topic');
-                          setTopic(+new Date());
-                        }}
-                        variant="outline"
-                        className="cursor-pointer hover:bg-accent"
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select a verified email to display" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {DIFFICULTIES?.length ? (
+                            DIFFICULTIES?.map((d) => (
+                              <Fragment key={d}>
+                                <SelectItem className="cursor-pointer" value={d}>
+                                  {d}
+                                </SelectItem>
+                              </Fragment>
+                            ))
+                          ) : (
+                            <div>Error loading schools</div>
+                          )}
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="topic"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="itms-center flex justify-between">
+                        <span>Topic</span>
+                        <Badge
+                          onClick={() => {
+                            form.resetField('topic');
+                            setTopic(+new Date());
+                          }}
+                          variant="outline"
+                          className="cursor-pointer hover:bg-accent"
+                        >
+                          Clear
+                        </Badge>
+                      </FormLabel>
+                      <Select
+                        disabled={loading}
+                        key={topic}
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
                       >
-                        Clear
-                      </Badge>
-                    </FormLabel>
-                    <Select
-                      disabled={loading}
-                      key={topic}
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder="Select a verified email to display" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        {TOPICS?.length ? (
-                          TOPICS?.map((d) => (
-                            <Fragment key={d}>
-                              <SelectItem className="cursor-pointer" value={d}>
-                                {d}
-                              </SelectItem>
-                            </Fragment>
-                          ))
-                        ) : (
-                          <div>Error loading schools</div>
-                        )}
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button className="w-full" type="submit">
-                Find Match
-              </Button>
-            </div>
-          </form>
-        </Form>
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select a verified email to display" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {TOPICS?.length ? (
+                            TOPICS?.map((d) => (
+                              <Fragment key={d}>
+                                <SelectItem className="cursor-pointer" value={d}>
+                                  {d}
+                                </SelectItem>
+                              </Fragment>
+                            ))
+                          ) : (
+                            <div>Error loading schools</div>
+                          )}
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button disabled={loading} className="w-full" type="submit">
+                  Find Match
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- peerprep-fe/src/pages/index.tsx
+++ peerprep-fe/src/pages/index.tsx
@@ -1,3 +1,59 @@
+import { ArrowRight } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+import { useSession } from '@/context/useSession';
+
 export default function Home() {
-  return null;
+  const { sessionData } = useSession();
+
+  return (
+    <div className="layout">
+      <div className="flex flex-col items-start gap-2 px-4 py-8">
+        <h1 className="text-3xl font-bold">PeerPrep</h1>
+        <p className="text-lg font-light text-foreground">
+          Prepare smarter for technical interviews with PeerPrep. Find a peer, collaborate in
+          real-time, and build confidence for your dream job.
+        </p>
+        <div className="flex w-full items-center justify-start gap-2 py-2">
+          <Link href={`${sessionData?.user ? '/find-match' : '/auth/signin'}`}>
+            <Button size="sm">Get Started</Button>
+          </Link>
+          <Link href="/find-match">
+            <Button variant="ghost" size="sm">
+              Start coding
+              <ArrowRight className="ml-2 h-4 w-4" />
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <div className="overflow-hidden rounded-lg border bg-background shadow">
+        <Image src="/editor.png" alt="Code editor page" width={1600} height={800} />
+      </div>
+      <div className="py-6">
+        <p className="text-balance text-sm leading-loose text-muted-foreground">
+          Design copied from&nbsp;
+          <a
+            href="https://twitter.com/shadcn"
+            className="font-medium underline underline-offset-4"
+            target="_blank"
+            rel="noreferrer"
+          >
+            shadcn
+          </a>
+          . The source code for this project is available on&nbsp;
+          <a
+            href="https://github.com/CS3219-AY2425S1/cs3219-ay2425s1-project-g32"
+            className="font-medium underline underline-offset-4"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
 }
