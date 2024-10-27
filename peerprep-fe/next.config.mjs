b29db0308c8a3e03d24d3b156b003714faa98044
Tan Lip Wei <53000252+lipwei1808@users.noreply.github.com>
2024-10-27 11:47:57 +0800

    Add page extension for building

--- peerprep-fe/next.config.mjs
+++ peerprep-fe/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  pageExtensions: ['page.tsx', 'page.ts'],
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'lh3.googleusercontent.com', port: '' }],
   },
